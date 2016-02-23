7224b0288c0b68a3c8cdb94c75e0ab0d0f6a691d
Vitaliy Berov <vitberov@gmail.com>
2016-02-23 20:02:04 +0200

    Get into the DANGER ZONE!
    ... Poker style

--- keyboard/gh60/keymap_plain.c
+++ keyboard/gh60/keymap_plain.c
@@ -2,10 +2,18 @@
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
-    KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-           CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NO,  ENT,  \
-           LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,NO,  RSFT, \
-           LCTL,LGUI,LALT,          SPC,                     RALT,RGUI,APP, RCTL),
+    KEYMAP(CAPS, 1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    MINS, EQL,  BSPC, \
+           TAB,  Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    LBRC, RBRC, ENT,  \
+           ESC,  A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT, BSLS, NO,   \
+           LSFT, GRV,  Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, RSFT, NO,   \
+           LCTL, LALT, LGUI,             SPC,                    NO,   RGUI, FN0,  RALT, RCTL),
+    /* 1: Fn */
+    KEYMAP(NO,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,    F10,  F11,  F12,  NO, \
+           NO,   MPRV, MPLY, MNXT, NO,   NO,   NO,   NO,   NO,   NO,    NO,   NO,   NO,   NO, \
+           NO,   NO,   VOLD, VOLU, MUTE, NO,   LEFT, UP,   DOWN, RIGHT, NO,   NO,   NO,   NO, \
+           NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,   NO,    NO,   NO,   NO,   NO, \
+           NO,   NO,   NO,               NO,                     NO,    NO,   FN0,  NO,   NO),
 };
-const uint16_t PROGMEM fn_actions[] = {};
+const uint16_t PROGMEM fn_actions[] = {
+    [0] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
+};
--- keyboard/gh60/matrix.c
+++ keyboard/gh60/matrix.c
@@ -136,51 +136,48 @@ uint8_t matrix_key_count(void)
 
 /* Column pin configuration
  * col: 0   1   2   3   4   5   6   7   8   9   10  11  12  13
- * pin: F0  F1  E6  C7  C6  B6  D4  B1  B0  B5  B4  D7  D6  B3  (Rev.A)
- * pin:                                 B7                      (Rev.B)
+ * pin: F7  B6  B5  B4  D7  B2  B3  B7  D0  D1  D2  D3  C6  C7
  */
 static void  init_cols(void)
 {
     // Input with pull-up(DDR:0, PORT:1)
-    DDRF  &= ~(1<<0 | 1<<1);
-    PORTF |=  (1<<0 | 1<<1);
-    DDRE  &= ~(1<<6);
-    PORTE |=  (1<<6);
-    DDRD  &= ~(1<<7 | 1<<6 | 1<<4);
-    PORTD |=  (1<<7 | 1<<6 | 1<<4);
+    DDRF  &= ~(1<<7);
+    PORTF |=  (1<<7);
+    DDRD  &= ~(1<<7 | 1<<3 | 1<<2 | 1<<1 | 1<<0);
+    PORTD |=  (1<<7 | 1<<3 | 1<<2 | 1<<1 | 1<<0);
     DDRC  &= ~(1<<7 | 1<<6);
     PORTC |=  (1<<7 | 1<<6);
-    DDRB  &= ~(1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
-    PORTB |=  (1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
+    DDRB  &= ~(1<<7 | 1<<6 | 1<<5 | 1<<4 | 1<<3 | 1<<2);
+    PORTB |=  (1<<7 | 1<<6 | 1<<5 | 1<<4 | 1<<3 | 1<<2);
 }
 
 static matrix_row_t read_cols(void)
 {
-    return (PINF&(1<<0) ? 0 : (1<<0)) |
-           (PINF&(1<<1) ? 0 : (1<<1)) |
-           (PINE&(1<<6) ? 0 : (1<<2)) |
-           (PINC&(1<<7) ? 0 : (1<<3)) |
-           (PINC&(1<<6) ? 0 : (1<<4)) |
-           (PINB&(1<<6) ? 0 : (1<<5)) |
-           (PIND&(1<<4) ? 0 : (1<<6)) |
-           (PINB&(1<<1) ? 0 : (1<<7)) |
-           ((PINB&(1<<0) && PINB&(1<<7)) ? 0 : (1<<8)) |     // Rev.A and B
-           (PINB&(1<<5) ? 0 : (1<<9)) |
-           (PINB&(1<<4) ? 0 : (1<<10)) |
-           (PIND&(1<<7) ? 0 : (1<<11)) |
-           (PIND&(1<<6) ? 0 : (1<<12)) |
-           (PINB&(1<<3) ? 0 : (1<<13));
+    return (PINF&(1<<7) ? 0 : (1<<0)) |
+           (PINB&(1<<6) ? 0 : (1<<1)) |
+           (PINB&(1<<5) ? 0 : (1<<2)) |
+           (PINB&(1<<4) ? 0 : (1<<3)) |
+           (PIND&(1<<7) ? 0 : (1<<4)) |
+           (PINB&(1<<2) ? 0 : (1<<5)) |
+           (PINB&(1<<3) ? 0 : (1<<6)) |
+           (PINB&(1<<7) ? 0 : (1<<7)) |
+           (PIND&(1<<0) ? 0 : (1<<8)) |
+           (PIND&(1<<1) ? 0 : (1<<9)) |
+           (PIND&(1<<2) ? 0 : (1<<10)) |
+           (PIND&(1<<3) ? 0 : (1<<11)) |
+           (PINC&(1<<6) ? 0 : (1<<12)) |
+           (PINC&(1<<7) ? 0 : (1<<13));
 }
 
 /* Row pin configuration
  * row: 0   1   2   3   4
- * pin: D0  D1  D2  D3  D5
+ * pin: F0  F1  F2  F3  F5
  */
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRD  &= ~0b00101111;
-    PORTD &= ~0b00101111;
+    DDRF  &= ~0b01110011;
+    PORTF &= ~0b01110011;
 }
 
 static void select_row(uint8_t row)
@@ -188,24 +185,24 @@ static void select_row(uint8_t row)
     // Output low(DDR:1, PORT:0) to select
     switch (row) {
         case 0:
-            DDRD  |= (1<<0);
-            PORTD &= ~(1<<0);
+            DDRF  |= (1<<0);
+            PORTF &= ~(1<<0);
             break;
         case 1:
-            DDRD  |= (1<<1);
-            PORTD &= ~(1<<1);
+            DDRF  |= (1<<1);
+            PORTF &= ~(1<<1);
             break;
         case 2:
-            DDRD  |= (1<<2);
-            PORTD &= ~(1<<2);
+            DDRF  |= (1<<4);
+            PORTF &= ~(1<<4);
             break;
         case 3:
-            DDRD  |= (1<<3);
-            PORTD &= ~(1<<3);
+            DDRF  |= (1<<5);
+            PORTF &= ~(1<<5);
             break;
         case 4:
-            DDRD  |= (1<<5);
-            PORTD &= ~(1<<5);
+            DDRF  |= (1<<6);
+            PORTF &= ~(1<<6);
             break;
     }
 }
